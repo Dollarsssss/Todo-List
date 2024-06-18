2eec4b67f15c0ec76157da2cadc568ecdb0aa3ab
Dollars <aomsup5185@gmail.com>
2024-06-18 16:59:29 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='container-wrapper'>
+        <main>
+          <header className='todo-title'>
+            <div className='todo-title-wrapper'>
+              <h1>Todo List</h1>
+              <p>A simple todo React List App</p>
+            </div>
+          </header>
+          <section className='todo-task'>
+            <div className='todo-task-wrapper'>
+              <div className='todo-task-content'>
+                <div className='todo-task-title'>
+                    <p>task 1</p>
+                </div>
+                <div className='todo-task-icon'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className='new-todo'>
+            <div className='new-todo-wrapper'>
+              <div className='new-todo-title'>
+                  New todo
+              </div>
+              <div className='new-todo-menu'>
+                <input className='new-todo-input' placeholder='New Todo'/>
+                <button className='new-todo-btn'>
+                  ADD TODO
+                </button>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
